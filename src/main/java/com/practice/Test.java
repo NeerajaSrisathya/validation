357c1cd13559b58e7974801da5f4dfd0fefcaa39
srisathya <srisathya@192.168.0.23>
2019-06-10 01:16:58 -0700

    stash done

--- src/main/java/com/practice/Test.java
+++ src/main/java/com/practice/Test.java
@@ -3,12 +3,11 @@ package com.practice;
 public class Test {
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
+
 		System.out.println("Started stash");
-=======
+
 		System.out.println("hello");
 		System.out.println("hi github");
->>>>>>> stash
 		
 	}
 
