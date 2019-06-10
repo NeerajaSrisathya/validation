9579d0790cfb15d9b758498acebe76e9a3e5d732
srisathya <srisathya@192.168.0.23>
2019-06-10 13:43:53 -0700

    student is done

---
+++ src/main/java/com/practice/Student.java
@@ -0,0 +1,27 @@
+package com.practice;
+
+public class Student {
+	private int stdid;
+	private String stdname;
+	private String stdclg;
+	
+	public String getStdclg() {
+		return stdclg;
+	}
+	public void setStdclg(String stdclg) {
+		this.stdclg = stdclg;
+	}
+	public int getStdid() {
+		return stdid;
+	}
+	public void setStdid(int stdid) {
+		this.stdid = stdid;
+	}
+	public String getStdname() {
+		return stdname;
+	}
+	public void setStdname(String stdname) {
+		this.stdname = stdname;
+	}
+
+}
