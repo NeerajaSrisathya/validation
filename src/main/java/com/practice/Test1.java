7dc9dce6e1f148e220d81a04a6db40ec546ef4d7
srisathya <srisathya@192.168.0.23>
2019-06-10 01:10:13 -0700

    done

--- src/main/java/com/practice/Test1.java
+++ src/main/java/com/practice/Test1.java
@@ -3,7 +3,7 @@ package com.practice;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("start here");
-		System.out.println("stash started here");
+		System.out.println("stash started here@123");
 
 	}
 
