1ab531cb77d524d62f15a12ec060762d16670446
srisathya <srisathya@192.168.0.23>
2019-06-09 23:37:13 -0700

    student

---
+++ src/main/java/com/practice/Student.java
@@ -0,0 +1,27 @@
+package com.practice;
+
+public class Student {
+	public int stdid;
+	public String stdName;
+	public String clg;
+	
+	public String getClg() {
+		return clg;
+	}
+	public void setClg(String clg) {
+		this.clg = clg;
+	}
+	public int getStdid() {
+		return stdid;
+	}
+	public void setStdid(int stdid) {
+		this.stdid = stdid;
+	}
+	public String getStdName() {
+		return stdName;
+	}
+	public void setStdName(String stdName) {
+		this.stdName = stdName;
+	}
+
+}
