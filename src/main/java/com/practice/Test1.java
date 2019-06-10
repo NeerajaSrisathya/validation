7e4d1d07bf7d15dd75d793690ea38741fb471cbc
srisathya <srisathya@192.168.0.23>
2019-06-10 01:09:07 -0700

    done

---
+++ src/main/java/com/practice/Test1.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("start here");
+		System.out.println("stash started here");
+
+	}
+
+}
